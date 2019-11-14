bc4fa7612bd13029944e99229a017081247279af
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-11-14 16:53:57 +0000

    update varchar fields to text

--- resources/migrations/20191113133102-add-users-table.up.sql
+++ resources/migrations/20191113133102-add-users-table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE USERS(
-   ID                   INT PRIMARY KEY NOT NULL,
-   FIRST_NAME           CHAR(300) NOT NULL,
-   LAST_NAME            CHAR(300) NOT NULL,
-   EMAIL                CHAR(300) NOT NULL,
-   PASSWORD             CHAR(300) NOT NULL,
+   ID                   SERIAL PRIMARY KEY NOT NULL,
+   FIRST_NAME           text NOT NULL,
+   LAST_NAME            text NOT NULL,
+   EMAIL                text NOT NULL,
+   PASSWORD             text NOT NULL,
    CREATED_AT           timestamp NOT NULL,
    UPDATED_AT           timestamp NOT NULL
 );
