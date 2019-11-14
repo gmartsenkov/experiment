d57f600cc4e14486939706bd0400597f67930ac5
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-11-14 16:55:28 +0000

    adds user repo create method

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:dev {:env
-       {:database-url "postgresql://localhost/projectx_dev?user=projectx&password=project"}}
+       {:database-url "postgresql://localhost/projectx_test?user=projectx&password=project"}}
 
  :test {:env
-        {:database-url "postgresql://localhost/projectx_dev?user=projectx&password=project"}}}
+        {:database-url "postgresql://localhost/projectx_test?user=projectx&password=project"}}}
--- src/experiment/repositories/user.clj
+++ src/experiment/repositories/user.clj
@@ -1,4 +1,8 @@
 (ns experiment.repositories.user
-  (:require [clojure.java.jdbc :as jdbc]))
+  (:require [clojure.java.jdbc :as jdbc]
+            [experiment.core :refer [database]]
+            [experiment.repositories.helpers :refer :all]))
 
-(defn create [])
+(defn create [attributes]
+  "Inserts a user into the database"
+  (first (jdbc/insert! database :users (add-timestamps attributes))))
