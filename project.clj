06378d476e9bef6195135f8c7aacb4175bd0609e
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-11-21 17:23:16 +0000

    standalone

--- project.clj
+++ project.clj
@@ -20,6 +20,7 @@
             [migratus-lein "0.7.2"]
             [lein-environ "1.1.0"]]
   :ring {:handler experiment.handler/app}
+  :uberjar-name "experiment-standalone"
   :migratus {:store :database
            :migration-dir "migrations"
              :db (get (System/getenv) "DATABASE_URL")}
