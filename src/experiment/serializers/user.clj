210c64fc0f9f4e492c1518ad6bd5912c9e74693e
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-12-01 23:28:55 +0000

    adds user serializer

---
+++ src/experiment/serializers/user.clj
@@ -0,0 +1,7 @@
+(ns experiment.serializers.user
+  (:require [experiment.services.serializer :as s]))
+
+(def options {:type "users"
+              :attributes '(:first_name :last_name :email)})
+
+(defn serialize [obj] (s/serialize obj options))
---
+++ test/experiment/serializers/user_test.clj
@@ -0,0 +1,14 @@
+(ns experiment.serializers.user-test
+  (:require [clojure.test :refer :all]
+            [experiment.serializers.user :refer :all]))
+
+(deftest test-serialize
+  (testing "serializes the user in JSONAPI format"
+    (let [user {:id 1 :first_name "Jon" :last_name "Snow" :email "jon@snow"}]
+      (is (= {:id 1
+              :type "users"
+              :attributes {
+                           :first_name "Jon"
+                           :last_name "Snow"
+                           :email "jon@snow"}}
+             (serialize user))))))
