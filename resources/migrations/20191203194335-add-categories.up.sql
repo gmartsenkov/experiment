1d55f96ce14d935801a1979944db218e544965c0
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-12-05 01:01:32 +0000

    migration to populate categories

--- resources/migrations/20191203194335-add-categories.up.sql
+++ resources/migrations/20191203194335-add-categories.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE CATEGORIES(
    ID                   SERIAL PRIMARY KEY NOT NULL,
    NAME                 text NOT NULL,
-   CREATED_AT           timestamp NOT NULL,
-   UPDATED_AT           timestamp NOT NULL
+   CREATED_AT           timestamp NOT NULL DEFAULT NOW(),
+   UPDATED_AT           timestamp NOT NULL DEFAULT NOW()
 );
